2bc5c73a5481121418327c5c990287fc0bfede4c
IoanMamsurov <ivan.mamsurov@yandex.ru>
2024-08-13 22:15:30 +0300

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,4 @@
+number_of_homeworks=12
+numder_of_hours=1.5
+course_name='Python'
+print('Курс ',course_name,' всего задач: ',number_of_homeworks,' затрачено часов: ',numder_of_hours, 'среднее время выполнения: ',numder_of_hours/number_of_homeworks)
